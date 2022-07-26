cf7220bd1472ab2a4fca6d6b9a1151d798baabe4
Henning Le Roux <henning.leroux@developersinstitute.co.nz>
2022-07-26 17:15:17 +1200

    added my pics to project and about component jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,56 @@
 import React from "react";
 import "./About.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      About
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Me" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about_card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>6 Months Study</small>
+            </article>
+            <article className="about_card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>0</small>
+            </article>
+            <article className="about_card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>1</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit id
+            iste iure nesciunt quis voluptatibus libero, magni commodi veritatis
+            eos, repellat maxime, asperiores sapiente nulla expedita! Accusamus
+            doloribus quidem rem.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Lets Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
